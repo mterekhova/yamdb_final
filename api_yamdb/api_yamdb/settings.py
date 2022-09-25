ea9f13647864c0c6809a964a134ba4e1be5d99f8
РњР°СЂРёСЏ РўРµСЂРµС…РѕРІР° <m.terekhova@itfm.ru>
2022-09-25 23:20:06 +0300

    9

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -62,11 +62,11 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME', default='postgresql'),
+        'USER': os.getenv('POSTGRES_USER', default='postgresql'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgresql'),
+        'HOST': os.getenv('DB_HOST', default='postgresql'),
+        'PORT': os.getenv('DB_PORT', default='5432'),
     }
 }
 
